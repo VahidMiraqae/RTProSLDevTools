01ee5492f5e07b4b4f3996aaa192611971da9527
Vahid Miraqae <vahidmiraqae@gmail.com>
2024-09-22 01:21:32 +0330

    ۱

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -20,6 +20,9 @@ namespace RTProSLDevTools.Data
         [DbFunction(IsBuiltIn = true)]
         public static string TYPE_NAME(int object_id) => throw new InvalidOperationException();
 
+        [DbFunction(IsBuiltIn = true)]
+        public static int OdbcScale(byte? param1, byte? param2) => throw new InvalidOperationException();
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<SpParameter>(e =>
@@ -30,6 +33,23 @@ namespace RTProSLDevTools.Data
                 e.Property(a => a.MaxLength).HasColumnName("max_length");
                 e.Property(a => a.SystemTypeId).HasColumnName("system_type_id");
                 e.Property(a => a.Precision).HasColumnName("precision");
+                e.Property(a => a.ParameterId).HasColumnName("parameter_id");
+                e.Property(a => a.UserTypeId).HasColumnName("user_type_id");
+                e.Property(a => a.Scale).HasColumnName("scale");
+                e.Property(a => a.IsOutput).HasColumnName("is_output");
+                e.Property(a => a.IsCursorRef).HasColumnName("is_cursor_ref");
+                e.Property(a => a.HasDefaultValue).HasColumnName("has_default_value");
+                e.Property(a => a.IsXmlDocument).HasColumnName("is_xml_document");
+                e.Property(a => a.DefaultValue).HasColumnName("default_value").HasColumnType("sql_varient")
+                .HasConversion(v => 1, v => 2);
+                e.Property(a => a.XmlCollectionId).HasColumnName("xml_collection_id");
+                e.Property(a => a.IsReadonly).HasColumnName("is_readonly");
+                e.Property(a => a.IsNullable).HasColumnName("is_nullable");
+                e.Property(a => a.EncryptionType).HasColumnName("encryption_type");
+                e.Property(a => a.EncryptionTypeDesc).HasColumnName("encryption_type_desc");
+                e.Property(a => a.EncryptionAlgorithmName).HasColumnName("encryption_algorithm_name");
+                e.Property(a => a.ColumnEncryptionKeyId).HasColumnName("column_encryption_key_id");
+                e.Property(a => a.ColumnEncryptionKeyDatabaseName).HasColumnName("column_encryption_key_database_name");
                 e.HasNoKey();
             });
         }
--- QueryHandlers/GetSpParameterQueryHandler.cs
+++ QueryHandlers/GetSpParameterQueryHandler.cs
@@ -21,6 +21,24 @@ public class GetSpParameterQueryHandler(AppDbContext appDbContext)
                                Name = e.Name,
                                MaxLength = e.MaxLength,
                                SystemTypeId = e.SystemTypeId,
+                               ParameterId = e.ParameterId,
+                               UserTypeId = e.UserTypeId,
+                               IsOutput = e.IsOutput,
+                               IsCursorRef = e.IsCursorRef,
+                               HasDefaultValue = e.HasDefaultValue,
+                               IsXmlDocument = e.IsXmlDocument,
+                               DefaultValue = e.DefaultValue,
+                               XmlCollectionId = e.XmlCollectionId,
+                               IsReadonly = e.IsReadonly,
+                               IsNullable = e.IsNullable,
+                               EncryptionType = e.EncryptionType,
+                               EncryptionTypeDesc = e.EncryptionTypeDesc,
+                               EncryptionAlgorithmName = e.EncryptionAlgorithmName,
+                               ColumnEncryptionKeyId = e.ColumnEncryptionKeyId,
+                               ColumnEncryptionKeyDatabaseName = e.ColumnEncryptionKeyDatabaseName,
+                               Scale = AppDbContext.OdbcScale(e.SystemTypeId, e.Scale),
+                               TypeName = AppDbContext.TYPE_NAME(e.SystemTypeId),
+                               //Collation = 
                                Precision = AppDbContext.TYPE_NAME(e.SystemTypeId) == "uniqueidentifier"
                                    ? e.Precision
                                    : AppDbContext.OdbcPrec(e.SystemTypeId, e.MaxLength, e.Precision!.Value)
